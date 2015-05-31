035b1ad55e5b080ed6db4d0914bda8f1ecada270
steve.sturtevant <steve.sturtevant@appdynamics.com>
2015-05-31 11:47:09 -0500

    checking in load test

---
+++ appdynamics-paypal-integration-controller/src/test/java/com/appdynamics/samples/AppTest.java
@@ -0,0 +1,76 @@
+package com.appdynamics.samples;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	private final static int TEST_DURATION_SECS=900;
+	private Date testStart = new Date();
+
+	private ArrayList<String> urlList = new ArrayList<String> ();
+
+	public AppTest()
+	{
+		urlList.add("http://127.0.0.1:8081/paypal-online-store");
+		urlList.add("http://127.0.0.1:8081/paypal-online-store/addcard");
+		urlList.add("http://127.0.0.1:8081/paypal-online-store/checkout");
+		urlList.add("http://127.0.0.1:8081/paypal-online-store/accountHistory");
+	}
+
+	private boolean shouldTest() {
+		long start = testStart.getTime() / 1000;
+		long now = new Date().getTime() / 1000;
+
+		return (now - start < TEST_DURATION_SECS) ? true : false;
+
+	}
+
+	@Test
+	public void TestController() {
+		boolean success = true;
+		try {
+			while (shouldTest()) {
+				for (String address : urlList) {
+					hitUrl(address);
+				}
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+			success = false;
+		}
+
+		assertTrue(success);
+	}
+
+	private void hitUrl(String address) throws Exception {
+		URL url = new URL(address);
+
+		URLConnection urlConn = url.openConnection();
+		
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+						urlConn.getInputStream()));
+		String inputLine;
+
+		String page = "";
+		while ((inputLine = in.readLine()) != null) {
+			page += inputLine;
+		}
+		System.out.println(page);
+		
+		in.close();
+	}
+
+}
